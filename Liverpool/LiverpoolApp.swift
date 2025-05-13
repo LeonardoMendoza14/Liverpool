2de4ede667033a76b62541ace4a9c2a609460fbc
Leonardo <leodanielmp@gmail.com>
2025-05-13 00:05:45 -0600

    Initial Commit

---
+++ Liverpool/LiverpoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiverpoolApp.swift
+//  Liverpool
+//
+//  Created by Mac OS lab on 13/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LiverpoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
